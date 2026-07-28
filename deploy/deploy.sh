d9d6cbd839b22bd0d72f7e220552890cb6c5a2e8
teobgeno <teobgeno@gmail.com>
2026-07-28 17:34:49 +0300

    -

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,9 @@ set -e
 
 cd /home/ec2-user/app
 
+echo "=== Logging in to ECR ==="
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 958085379190.dkr.ecr.us-east-1.amazonaws.com
+
 echo "=== Pulling latest images ==="
 docker compose -f docker-compose.prod.yml pull backend frontend
 
