7915796ede139ee287d2374979ec7d55621843f9
teobgeno <teobgeno@gmail.com>
2026-07-29 14:03:02 +0300

    db ssl connection

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -8,6 +8,9 @@ export default new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
